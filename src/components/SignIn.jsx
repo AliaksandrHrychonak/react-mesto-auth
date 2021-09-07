c8dfd146447aa3c30fbd4906880e366a42ea188b
Aliaksandr Hrychonak <alexsanderkorn90@gmail.com>
2021-09-07 13:33:24 +0300

    added markup of access forms

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
-
+import { Link } from "react-router-dom";
 export const SignIn = () => {
+
   return (
-    <div>
-      
-    </div>
+    <section className="access">
+      <form className="access__form">
+        <h1 className="access__title">
+          Вход
+        </h1>
+        <input type="email" className="access__form-input"/>
+        <input type="email" className="access__form-input"/>
+        <button className="access__button-submit" type="submit"></button>
+      </form>
+      <p className="access__option">Уже зарегистрированы?
+      <Link to="sign-in" className="access__login-link">Войти</Link>
+      </p>
+    </section>
   );
 }
 
